0ca9dca9e1668b0ab4d6e384b7aec234d2bc8575
tb1121 <Taylorball5@yahoo.com>
2023-05-10 19:34:05 -0700

    streamlined code

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,31 +3,17 @@ import { Route, Routes, useNavigate} from "react-router-dom";
 import LoginPage from './LoginPage';
 import MainPage from './MainPage';
 
-export const Context = createContext('');
 
-
-//useState for signedIn
-
-
+  
 const App = () => {
-    const [loggedIn, setLoggedIn] = useState('true');
+    
     const navigate = useNavigate();
-    useEffect(()=>{
-    if (loggedIn === 'true'){
-        navigate('/mainPage');
-    }
-    else{
-        navigate('/login');
-    }
-}, [loggedIn, navigate])
-    //wrap routes in Context.Provider value ={loggedIn, setLoggedIn}
+
     return (
-        <Context.Provider value={loggedIn}>
         <Routes>
             <Route path='/login' element={<LoginPage/>} caseSensitive={true} />
             <Route path='/mainPage' element={<MainPage/>} caseSensitive={true}/>
         </Routes>
-        </Context.Provider>
     )
 }
   
