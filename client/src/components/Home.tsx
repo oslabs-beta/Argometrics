1978b0eea30f724e21438fc8130a2d6562db71d7
tb1121 <Taylorball5@yahoo.com>
2023-05-13 14:34:31 -0700

    worked on styling

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,9 +4,9 @@ import '../stylesheets/Home.scss'
 const Home = () =>{
   return(
     <>
-    <div id='homepage'>
-      <h3>Add Cluster</h3>
-      <button>+</button>
+    <div id="homepage">
+      <h1  className="fa-fade">Add Cluster</h1>
+    <button id="cluster-btn" className="fa-thin fa-plus fa-fade fa-xl"></button>
     </div>
     </>
   )
