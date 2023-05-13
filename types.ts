2c5395ad21472014632d51f6162cfc4386bd47b7
Ryan Sun <ryansun222@gmail.com>
2023-05-13 15:46:37 -0700

    created metrics file for metrics we will be monitoring in our application through grafana, editted types.ts file

--- types.ts
+++ types.ts
@@ -7,7 +7,39 @@ export type Cluster = {
                 // ex output: "http://localhost:3000/"
     url: string,
     // 
-    dashboards: string
+    dashboards: DashboardUIds
+}
+
+
+export type DashboardUIds = {
+  apiServerUId: {
+    dashboardUIDKey: string,
+    grafanaLinkDText: string
+  },
+  kubeStateMetricUId: {
+    dashboardUIDKey: string,
+    grafanaLinkDText: string
+  },
+  kubePrometheusUId: {
+    dashboardUIDKey: string,
+    grafanaLinkDText: string
+  },
+  nodeExporterUId: {
+    dashboardUIDKey: string,
+    grafanaLinkDText: string
+  },
+  prometheusExporterUId: {
+    dashboardUIDKey: string,
+    grafanaLinkDText: string
+  },
+}
+
+export type DBUIds = {
+  apiServerUId?: string,
+  kubeStateMetricUId?: string,
+  kubePrometheusUId?: string,
+  nodeExporterUId?: string,
+  prometheusExporterUId?: string,
 }
 
 // /d
